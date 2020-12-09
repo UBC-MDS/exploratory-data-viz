36bd7f924ff48609f0463272e2ba245f57b872ec
Joel Ostblom <joel.ostblom@gmail.com>
2020-12-09 15:57:18 -0800

    Add exercise files for 1.3

---
+++ exercises/en/exc_01_06.py
@@ -0,0 +1,10 @@
+import altair as alt
+from vega_datasets import ___
+
+
+stocks = ___
+
+alt.Chart(___).___.encode(
+    x=___,
+    y=___,
+    color=___)
---
+++ exercises/en/test_01_08.py
@@ -0,0 +1,14 @@
+def test():
+    # Here we can either check objects created in the solution code, or the
+    # string value of the solution, available as __solution__. A helper for
+    # printing formatted messages is available as __msg__. See the testTemplate
+    # in the meta.json for details.
+    # If an assertion fails, the message will be displayed
+
+    # Since we haven't started assigning charts to variable names yet,
+    # this might be the better way to test for the first exercise.
+    # Maybe even for later exercises.
+
+    # TODO implement test
+    __msg__.good("Not yet implemented")
+    # __msg__.good("You're correct, well done!")
