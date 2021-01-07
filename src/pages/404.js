9e481239630c8d78f43c632d26e5b8b7d5ff04fc
Joel Ostblom <joel.ostblom@gmail.com>
2021-01-07 10:23:11 -0800

    Change 404 page to fix CI build error

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,10 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
-
-export default NotFoundPage
+const NotFoundPage = () => {
+  return <div><h1>404</h1>
+        <p>
+        This page does not exist. <a href="/">Click here to go back to the homepage.</a>
+        </p>
+        </div>
+}
+export default NotFoundPage;
