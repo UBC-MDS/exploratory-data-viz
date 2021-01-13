c328bd65c599652e20d0f3c40cdad0e930d96abe
Junghoo Kim <jkim222383@gmail.com>
2021-01-13 01:29:10 -0800

    Include tests for module 1 exercises.

--- exercises/en/test_01_06.py
+++ exercises/en/test_01_06.py
@@ -8,6 +8,8 @@ def test():
     # Since we haven't started assigning charts to variable names yet,
     # this might be the better way to test for the first exercise.
     # Maybe even for later exercises.
+    assert 'alt.Chart(cars)' in __solution__, "Make sure you're passing the correct data object for plotting."
+    assert 'mark_point(' in __solution__, "Make sure you're using the correct type of mark"
     assert 'x="Weight_in_lbs"' in __solution__, "Did you encode the x channel correctly?"
     assert 'y="Horsepower"' in __solution__, "Did you encode the y channel correctly?"
     assert 'color="Origin"' in __solution__, "Did you encode the color channel correctly?"
--- exercises/en/test_01_09a.py
+++ exercises/en/test_01_09a.py
@@ -8,6 +8,11 @@ def test():
     # Since we haven't started assigning charts to variable names yet,
     # this might be the better way to test for the first exercise.
     # Maybe even for later exercises.
+    assert 'from vega_datasets import data' in __solution__, "Did you import the correct library for data from vega_datasets?"
+    assert 'data.stocks()' in __solution__, "Make sure to call the stocks data from the data library"
+    assert '# alt.Chart' not in __solution__, "Make sure you remove the hash (#) symbol used to comment out the plotting codes."
+    assert 'alt.Chart(stocks)' in __solution__, "Make sure you're passing the correct data object fot plotting."
+    assert 'mark_line' in __solution__, "Make sure you're using the correct type of mark"
     assert 'x="date"' in __solution__, "Did you encode the x channel correctly?"
     assert 'y="price"' in __solution__, "Did you encode the y channel correctly?"
     assert 'color="symbol"' in __solution__, "Did you encode the color channel correctly?"
