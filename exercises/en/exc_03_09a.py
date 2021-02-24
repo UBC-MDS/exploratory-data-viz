45c671af017c0ca9be01d1baea95a53897237f81
hfboyce <hayleyfboyce@gmail.com>
2021-02-24 15:38:46 -0800

    added fixes from tiffany per #51

--- exercises/en/exc_03_09a.py
+++ exercises/en/exc_03_09a.py
@@ -2,16 +2,16 @@ import altair as alt
 import pandas as pd
 
 
-penguins = pd.read_csv('data/penguins.csv')
+penguins_df = pd.read_csv('data/penguins.csv')
 
 mass_density_plot = alt.Chart(___).transform_density(
     ___,
-    ___,
-    ___,
-    ___
+    groupby=___,
+    ___=['body_mass_g', 'density'],
+    ___=100
     ).mark_area(___).encode(
+        x=___,
         ___,
-        ___,
-        ___).___
+        colour=___).properties(___)
 
 mass_density_plot
--- exercises/en/solution_03_04.py
+++ exercises/en/solution_03_04.py
@@ -2,10 +2,10 @@ import altair as alt
 import pandas as pd
 
 
-penguins = pd.read_csv('data/penguins.csv').dropna(subset=['sex'])
+penguins_df = pd.read_csv('data/penguins.csv').dropna(subset=['sex'])
 
-culmen_stacked_plot = alt.Chart(penguins).mark_bar().encode(
-    alt.X('culmen_depth_mm', bin=alt.Bin(maxbins=30)),
+culmen_stacked_plot = alt.Chart(penguins_df).mark_bar().encode(
+    alt.X('culmen_depth_mm', bin=alt.Bin(maxbins=30), title='Culmen depth (mm)'),
     alt.Y('count()'),
     color='species'
     ).properties(title="Penguin culmen depth distributions among species", width=230, height=200
