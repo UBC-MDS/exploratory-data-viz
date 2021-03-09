cd48cf3bf63b7f921a9fd7fee1300de22d245ad7
Joel Ostblom <joel.ostblom@gmail.com>
2021-03-08 16:56:10 -0800

    Make all links open in new tab

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+      <base target="_blank" />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
