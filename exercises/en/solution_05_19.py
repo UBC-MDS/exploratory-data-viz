750c87d0453996b7ac56c71ca2168359cb6b2813
Joel Ostblom <joelostblom@users.noreply.github.com>
2021-03-16 09:12:28 -0700

    5.1 effective figure design (#65)
    Add slides for module 5
    
    * Add skeleton files
    
    * Add 5.1 slides
    
    * Add images
    
    * Update index for all module 5 decks
    
    * Add section on breaking axes to 5.1
    
    * Increase slide height for easier review
    
    Might implement a different finl solution
    
    * Add 5.2 slides
    
    * Add 5.1 slide image of broken axis
    
    * Add 5.3 slides with both wiki and stock for first half
    
    * Change 5.3 to use wikipedia data throughout
    
    * Convert no-show cells back to code
    
    * Fix typo
    
    * Add module 4, 5, and 6 to index
    
    * Add slide deck 5.4
    
    * Add images for module 5.4
    
    * Add bck code cell that wont run with vegawidget/knitr
    
    * gap
    
    * Add intro slides on color HSL
    
    * Add images for intro slides
    
    * Shrink image
    
    * Add slides for 5.5
    
    * Add weather data
    
    * Add coor scheme lightness img
    
    * Add weather data source
    
    * Add notes about additional formatters in 5.2
    
    * Add slides for 5.6
    
    * first round of interactive qs
    
    * added images
    
    * fixed up first round, still have 1 q left
    
    * updated as per #121
    
    * Update index for all module 5 decks
    
    * Add section on breaking axes to 5.1
    
    * Increase slide height for easier review
    
    Might implement a different finl solution
    
    * Add 5.2 slides
    
    * Add 5.1 slide image of broken axis
    
    * Bring files up date from main
    
    Co-authored-by: hfboyce <hayleyfboyce@gmail.com>

---
+++ exercises/en/solution_05_19.py
@@ -0,0 +1,18 @@
+import altair as alt
+import pandas as pd
+
+
+temps_df = pd.read_csv('data/temperature.csv', parse_dates=['date'])
+
+temp_plot = alt.Chart(temps_df).mark_circle(size=50).encode(
+    alt.X('date', 
+        title="Date"),
+    alt.Y('total_rain_mm',
+        title='rainfall total for the month (mm)'),
+    alt.Color('mean_temp',
+        title=' Mean Temperature',
+        scale=alt.Scale(scheme='blueorange'))
+    ).properties(title='Hotter months tend to have higher levels of rainfall')
+
+temp_plot
+
