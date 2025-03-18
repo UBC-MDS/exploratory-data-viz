d17897fa7b70b324813a3e437673a82a50c3a1b6
Sandra Gross <sandra-starnberg@web.de>
2025-03-17 20:48:33 -0700

    Added Module 6 Subchapter Figure Layouts

--- modules/utils.py
+++ modules/utils.py
@@ -3,15 +3,21 @@ import random
 import altair as alt
 
 
-@alt.theme.register("increase_font_size", enable=True)
-def increase_chart_font_size() -> alt.theme.ThemeConfig:
+def increase_font_size():
     return {
-    'config': {
-        'view': {'continuousWidth': 400, 'continuousHeight': 300},
-        'legend': {'symbolSize': 14, 'titleFontSize': 14, 'labelFontSize': 14},
-        'axis': {'titleFontSize': 14, 'labelFontSize': 12},
-        'header': {'titleFontSize': 16, 'labelFontSize': 14},
-        'encoding': {'x': {'scale': {'zero': False}}}}}
+        "config": {
+            "view": {"continuousWidth": 400, "continuousHeight": 300},
+            "legend": {"symbolSize": 14, "titleFontSize": 14, "labelFontSize": 14},
+            "axis": {"titleFontSize": 14, "labelFontSize": 12},
+            "header": {"titleFontSize": 16, "labelFontSize": 14},
+            "encoding": {"x": {"scale": {"zero": False}}},
+        }
+    }
+
+
+alt.themes.register("increase_font_size", increase_font_size)
+alt.themes.enable("increase_font_size")
+
 
 # def increase_chart_font_size():
 #     import altair as alt
@@ -27,8 +33,8 @@ def increase_chart_font_size() -> alt.theme.ThemeConfig:
 
 
 def assert_chart_equal(expected, actual):
-    expected_dict = expected.to_dict()
-    actual_dict = actual.to_dict()
+    expected_dict = expected if isinstance(expected, dict) else expected.to_dict()
+    actual_dict = actual if isinstance(actual, dict) else actual.to_dict()
     try:
         assert_dict_equal(expected_dict, actual_dict)
         message = random.choice(["Nicely done", "Great", "Good job", "Well done"])
